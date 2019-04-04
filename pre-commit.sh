3efcb2062c295a455114dc870881e54b9f54354d
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2019-04-04 15:21:28 -0500

    Only update the PGP key on my computer.

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # To register:
 # cd .git/hooks && ln -s ../../pre-commit.sh pre-commit
-gpg --armor --export e72640d09d0ca9eea09879c5468c80811bcc9a6a > pgp-key.txt
-git add pgp-key.txt
+if gpg --list-secret-keys --with-colons | grep -i ^fpr:.*:e72640d09d0ca9eea09879c5468c80811bcc9a6a: > /dev/null
+then
+    gpg --armor --export e72640d09d0ca9eea09879c5468c80811bcc9a6a > pgp-key.txt
+    git add pgp-key.txt
+fi

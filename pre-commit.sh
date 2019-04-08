8d88d1ab3920dcdbe69ba04e44a63aad2ca0d0d1
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2019-04-08 18:12:19 -0500

    Force manual updates to my PGP key.

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 # To register:
 # cd .git/hooks && ln -s ../../pre-commit.sh pre-commit
-if gpg --list-secret-keys --with-colons | grep -i ^fpr:.*:e72640d09d0ca9eea09879c5468c80811bcc9a6a: > /dev/null
+MY_PGP_KEY=e72640d09d0ca9eea09879c5468c80811bcc9a6a
+if gpg --list-secret-keys --with-colons | grep -i ^fpr:.*:$MY_PGP_KEY: > /dev/null
 then
-    gpg --armor --export e72640d09d0ca9eea09879c5468c80811bcc9a6a > pgp-key.txt
-    git add pgp-key.txt
+    if ! gpg --armor --export $MY_PGP_KEY | cmp --silent pgp-key.txt
+    then
+        echo ERROR: PGP key has been changed. Run the following command to update the local copy:
+        echo gpg --armor --export $MY_PGP_KEY ">" pgp-key.txt
+        exit 1
+    fi
 fi

af5f23fc0185179e8ba9693646bd02517d0d7f19
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2021-12-09 17:51:10 -0600

    Make analytics domain configurable.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-echo "url: http://localhost:4000" > .jekyll-cache/jb-dev-config.yml
+test -n "$JB_ANALYTICS_DOMAIN" || JB_ANALYTICS_DOMAIN=
+cat - > .jekyll-cache/jb-dev-config.yml << EOF
+jbanalytics: "$JB_ANALYTICS_DOMAIN"
+EOF
 (cd .git/hooks && ln -s ../../pre-commit.sh pre-commit 2>/dev/null) || true
-bundle exec jekyll serve --config _config.yml,.jekyll-cache/jb-dev-config.yml
+bundle exec jekyll serve --config _config.yml,.jekyll-cache/jb-dev-config.yml --port 3999

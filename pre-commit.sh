a1f597fc50680746a2194ae14ce706a093434313
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2022-11-15 16:47:46 -0600

    Return internship, and fix GPG script.

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,7 +4,7 @@
 MY_PGP_KEY=e72640d09d0ca9eea09879c5468c80811bcc9a6a
 if gpg --list-secret-keys --with-colons | grep -i ^fpr:.*:$MY_PGP_KEY: > /dev/null
 then
-    if ! gpg --armor --export $MY_PGP_KEY | cmp --silent pgp-key.txt
+    if ! gpg --armor --export $MY_PGP_KEY | cmp --silent pgp-key.txt - > /dev/null
     then
         echo ERROR: PGP key has been changed. Run submit-gpg-key.sh to update.
         exit 1

9097e819a29890bf9ee656caff717f2e4eb66d8e
JBYoshi <12983479+JBYoshi@users.noreply.github.com>
2026-04-04 16:04:19 -0500

    Update PGP key.

--- submit-gpg-key.sh
+++ submit-gpg-key.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 MY_PGP_KEY=e72640d09d0ca9eea09879c5468c80811bcc9a6a
 
-gpg --keyserver hkp://pgp.mit.edu --send-keys $MY_PGP_KEY
-gpg --keyserver hkp://keyserver.ubuntu.com --send-keys $MY_PGP_KEY
-gpg --keyserver hkp://keys.openpgp.org --send-keys $MY_PGP_KEY
-
-keybase pgp update
-
-gpg --armor --export $MY_PGP_KEY > `dirname $0`/pgp-key.txt
+gpg --armor --export $MY_PGP_KEY > "$(dirname $0)"/pgp-key.txt
 gpg --armor --export $MY_PGP_KEY | pbcopy
 
-echo "The automatic steps are done. Manual steps:"
+echo "Manual steps:"
 echo "- Replace the GPG key on the following sites (it's copied to the clipboard):"
 echo "  - https://github.com/settings/keys"
 echo "  - https://gitlab.com/-/user_settings/gpg_keys"
 echo "- Commit and push pgp-key.txt"
+echo
+echo "Running automatic propagations..."
+
+keybase pgp update
+
+gpg --keyserver hkp://keys.openpgp.org --send-keys $MY_PGP_KEY
+gpg --keyserver hkp://keyserver.ubuntu.com --send-keys $MY_PGP_KEY
+gpg --keyserver hkp://pgpkeys.eu --send-keys $MY_PGP_KEY
+gpg --keyserver hkp://pgp.mit.edu --send-keys $MY_PGP_KEY
